6c9b993fdbb145b224f4e0cd8adc263083e5f2aa
Debasish <debasish@devopspro.com>
2025-06-13 21:52:05 +0530

    update

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "debasish-terraform-state-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
 resource "aws_vpc" "custom_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
