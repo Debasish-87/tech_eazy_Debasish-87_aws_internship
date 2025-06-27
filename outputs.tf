bb4ac59434410ec1ea6b94c6667164a10755e5f0
Debasish <debasish@devopspro.com>
2025-06-27 23:14:58 +0530

    Assignment-4: Added Terraform config for AWS infrastructure

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,4 @@
-output "vpc_id" {
-  value = aws_vpc.custom_vpc.id
+output "app_ec2_public_ip" {
+  description = "Public IP of the EC2 instance running the Java app"
+  value       = aws_instance.app_server.public_ip
 }
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "instance_public_ip" {
-  value = aws_instance.app.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.app.id
-}
-
-
--- s3.tf
+++ s3.tf
@@ -1,27 +1,41 @@
-resource "aws_s3_bucket" "logs_bucket" {
-  bucket = var.logs_bucket_name
+resource "aws_s3_bucket" "log_s3_bucket" {
+  count = var.stage =="dev" ? 1:0
+  bucket = var.log_s3_bucket_name
+  force_destroy = true
 
   tags = {
-    Name = "${var.environment}-logs"
+    Name = "LogBucket"
+    stage = var.stage
   }
-
-  force_destroy = true  
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "log_expiry" {
-  bucket = aws_s3_bucket.logs_bucket.id
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+ count = var.stage =="dev" ? 1:0
+ bucket = aws_s3_bucket.log_s3_bucket[0].id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "log_lifecycle" {
+  count = var.stage =="dev" ? 1:0
+  bucket = aws_s3_bucket.log_s3_bucket[0].id
 
   rule {
-    id     = "delete-logs-after-7-days"
+    id     = "delete_logs_after_7_days"
     status = "Enabled"
-
-    filter {
-      prefix = ""  
-    }
 
     expiration {
       days = 7
     }
+
+    filter {}
   }
 }
 
+# Reuse in prod if already created in dev
+data "aws_s3_bucket" "log_s3_bucket" {
+  count = var.stage =="prod" ? 1:0
+  bucket = var.log_s3_bucket_name
+}
